5aa8556895f130086ea94619814609617db0c806
faremax <faremax@163.com>
2024-06-15 23:43:55 +0800

    feat: 初始化

---
+++ packages/quick-start/index.ts
@@ -0,0 +1,19 @@
+// 安装 node nvm pnpm
+
+// 配置git
+// ssh-keygen -t rsa -C "faremax@163.com"
+
+// 安装全局npm
+
+// 安装软件
+
+// 安装 vscode 插件、同步配置
+
+// 登录 chrome
+
+
+// 插件化架构，支持命令式配置
+// static description
+// beforeInstall
+// await install
+// afterInstall
